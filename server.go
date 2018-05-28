8ef3c9d7a35bc3d3c229f444b671118c44fda0ac
kizzone <kizzone.acm@gmail.com>
2018-05-29 01:18:34 +0200

    Make adjustment to server

--- server.go
+++ server.go
@@ -16,13 +16,24 @@ type levelPosition struct {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	levelPs := levelPosition{XCord: rand.Intn(100), YCord: rand.Intn(100), ZCord: rand.Intn(100)}
+	//creating an array of levelPosition
+
+	var levelPositionArray = []levelPosition{
+		levelPosition{XCord: 0, YCord: 0, ZCord: 0},  //rappresenting the quiet state
+		levelPosition{XCord: 0, YCord: 20, ZCord: 0}, //rappresenting the pulled state
+		levelPosition{XCord: rand.Intn(100), YCord: rand.Intn(100), ZCord: rand.Intn(100)},
+	}
+
+	//levelPs := levelPosition{XCord: rand.Intn(100), YCord: rand.Intn(100), ZCord: rrand.Intn(100)}
 	/*fmt.Println("New X position: ", levelPs.XCord)
 	fmt.Println("New Y position: ", levelPs.YCord)
 	fmt.Println("New Z position: ", levelPs.ZCord)
 	*/
-	jsonMarsh, err := json.Marshal(&levelPs)
 
+	//return from 0 to n
+	i := rand.Intn(3)
+
+	jsonMarsh, err := json.Marshal(&levelPositionArray[i])
 	if err != nil {
 		fmt.Println(err)
 		return
