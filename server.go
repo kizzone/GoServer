ed47e4d4791a3109e3dfc9039b5032436f6b0b5a
kizzone <kizzone.acm@gmail.com>
2018-06-04 23:05:29 +0200

    semi-finished server

--- server.go
+++ server.go
@@ -8,14 +8,15 @@ import (
 	"net/http"
 )
 
+//rappresent the acceleration of the lever
+
 type levelPosition struct {
 	XCord int
 	YCord int
 	ZCord int
 }
 
-//richiesta post che cambia lo stato della richiesta
-
+//handler for http request
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	currentlevelPs := levelPosition{XCord: 0, YCord: 0, ZCord: 0}
@@ -61,77 +62,3 @@ func main() {
 	http.HandleFunc("/accelerometer", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-/*
-
-
-
-package main
-
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
-)
-
-type levelPosition struct {
-	XCord int
-	YCord int
-	ZCord int
-}
-
-//richiesta post che cambia lo stato della richiesta
-
-func handler(w http.ResponseWriter, r *http.Request) {
-
-	levelPs := levelPosition{XCord: 0, YCord: 0, ZCord: 0}
-
-	currentLever = &levelPs
-
-	switch r.Method {
-
-	case "GET":
-
-		var levelPositionArray = []levelPosition{
-			levelPosition{XCord: 0, YCord: 0, ZCord: 0},  //rappresenting the quiet state
-			levelPosition{XCord: 0, YCord: 20, ZCord: 0}, //rappresenting the pulled state
-			levelPosition{XCord: rand.Intn(100), YCord: rand.Intn(100), ZCord: rand.Intn(100)},
-		}
-
-		//levelPs := levelPosition{XCord: rand.Intn(100), YCord: rand.Intn(100), ZCord: rrand.Intn(100)}
-		/*fmt.Println("New X position: ", levelPs.XCord)
-		fmt.Println("New Y position: ", levelPs.YCord)
-		fmt.Println("New Z position: ", levelPs.ZCord)
-
-		//return from 0 to n
-		i := rand.Intn(3)
-
-		jsonMarsh, err := json.Marshal(&levelPositionArray[i])
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Fprintf(w, "%s", string(jsonMarsh))
-
-	case "POST":
-		fmt.Fprintf(w, "Not implemented yet")
-
-	default:
-		fmt.Fprintf(w, "Sorry only POST and GET method allow")
-	}
-
-	//creating an array of levelPosition
-
-}
-
-func main() {
-	http.HandleFunc("/accelerometer", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-
-
-
-*/
